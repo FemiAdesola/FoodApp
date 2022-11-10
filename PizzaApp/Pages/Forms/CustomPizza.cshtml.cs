9cab0f6dac4bd1db5571e8a4ddf2085456378be3
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-11-10 23:04:08 +0200

    Creates custom pizza functinality, pizzasmodel

--- PizzaApp/Pages/Forms/CustomPizza.cshtml.cs
+++ PizzaApp/Pages/Forms/CustomPizza.cshtml.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaApp.Models;
 
 namespace PizzaApp.Pages.Forms
 {
 	public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel Pizza  { get; set; }
         public void OnGet()
         {
+            
         }
     }
 }
