f7aefaf4d5f2bb06f4c8cd644a616f5a67ca73d4
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-11-11 18:35:02 +0200

    creates order list from database function and ui

---
+++ PizzaApp/Pages/Orders.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaApp.Data;
+using PizzaApp.Models;
+
+namespace PizzaApp.Pages
+{
+	public class OrdersModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        public List<PizzaOrder> PizzaOrders = new List<PizzaOrder>();
+
+        public OrdersModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void OnGet()
+        {
+            PizzaOrders = _context.PizzaOrders.ToList();
+
+        }
+    }
+}
